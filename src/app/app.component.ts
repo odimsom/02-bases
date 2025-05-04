d50b646dbfbf7d498bea9ead923cfdb21945f0c0
odimsom <20231065@itla.edu.do>
2025-05-04 15:09:12 -0400

    primeros cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+  title = 'Francisco Castro';
+  description = 'esto es una breve description'
 }
